a2b3021530eddbf7ce89eca8dcf9954ab3a8e908
Abhishek Yashwant Deshmukh <abhishek.deshmukh@rwth-aachen.de>
2018-10-19 19:36:22 +0200

    Organized code into a structure. Makefile created for compilation.

--- compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm *.mod
-ifort -g precision.f90 math.f90 pc_defs.f90 pc_database.f90 pc_func.f90 rk_defs.f90 solver_defs.f90 spray_defs.f90 rk_func.f90 solver_func.f90 spray_func.f90  parser.f90 cas.f90 -o cas
-
-rm *.mod
-ifort -O2 precision.f90 math.f90 pc_defs.f90 pc_database.f90 pc_func.f90 rk_defs.f90 solver_defs.f90 spray_defs.f90 rk_func.f90 solver_func.f90 spray_func.f90  parser.f90 cas.f90 -o cas_opt
